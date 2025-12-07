e21ece18876c62fa7418c3e86f9cbfa5e33bf365
byulrim <byulrim@gmail.com>
2025-12-07 13:34:57 +0900

    studying pythin w/ nadocoding(first time): anal-data-app project

---
+++ studies/nadocoding/ch2.py
@@ -0,0 +1,42 @@
+# print(5)
+
+# print(-10)
+# print(3.14)
+# print(1000)
+
+# print(5 + 3)
+# print(2 * 8)
+# print(6 / 3)
+# print(3 * (3 + 1))
+
+# print('풍선')
+# print("나비")
+# print("abcdefg")
+# print("10")
+# print("파이썬" * 3)
+
+# print(5 > 10)
+# print(5 < 10)
+
+# print(True)
+# print(False)
+
+# print(not(5 > 10))
+
+# name = "연탄이"
+# animal = "개"
+# age = 4
+# hobby = "산책"
+# is_male = True
+
+name = "해피"
+animal = "고양이"
+age = 4
+hobby = "낮잠"
+is_male = True
+
+print("반려동물을 소개해 주세요.")
+print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "(이)예요.")
+print(name + "이는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+print("연탄이는 수컷인가요?")
+print("네")
