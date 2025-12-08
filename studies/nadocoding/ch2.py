6cd3bde11c43965d41f83f34e26ccd9682ba2eb2
byulrim <byulrim@gmail.com>
2025-12-08 22:50:33 +0900

    studying pythin w/ nadocoding(completed chapter 2): anal-data-app project

--- studies/nadocoding/ch2.py
+++ studies/nadocoding/ch2.py
@@ -23,20 +23,60 @@
 
 # print(not(5 > 10))
 
-# name = "연탄이"
-# animal = "개"
-# age = 4
-# hobby = "산책"
+# name = "연탄이" # 이름
+# animal = "개" # 종류
+# age = 4 # 
+# hobby = "산책" # 
 # is_male = True
 
-name = "해피"
-animal = "고양이"
-age = 4
-hobby = "낮잠"
-is_male = True
+# name = "해피"
+# animal = "고양이"
+# age = 4
+# hobby = "낮잠"
+# is_male = True
 
-print("반려동물을 소개해 주세요.")
-print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "(이)예요.")
-print(name + "이는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
-print("연탄이는 수컷인가요?")
-print("네")
+# print("반려동물을 소개해 주세요.")
+# print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "(이)예요.")
+# hobby = "수영"
+# print(name + "(이)는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+# print("연탄이는 수컷인가요?")
+# print("네")
+# print(name, "는", age, "살이고,", hobby, "을 아주 좋아해요.")
+
+# print(int("3"))
+# print(int("3") + "입니다.")
+
+# print(int(3.5))
+# try:
+# print(int("삼"))  # ValueError: invalid literal for int() with base 10: '삼'
+# except ValueError as e:
+    # print(f"오류 발생: {e}")
+
+# print(float("3.5"))
+# print(float(3))
+
+# print(str(3) + "입니다.")
+# print(str(3.5) + "입니다.")
+
+# print(type(3))
+# print(type("3"))
+# print(type(3.5))
+# print(type(str(3)))
+
+"""
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+"""
+
+"""
+status = "상품 준비"
+# status = "배송 중"
+# status = "배송 완료"
+print("주문상태 : " + status)
+"""
