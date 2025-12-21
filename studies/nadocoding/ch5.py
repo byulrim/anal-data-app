8c458f3093f0cd58f20be132897fe7c476eb764a
byulrim <byulrim@gmail.com>
2025-12-22 04:51:42 +0900

    studying pythin w/ nadocoding(chapter 5; third time): anal-data-app project

--- studies/nadocoding/ch5.py
+++ studies/nadocoding/ch5.py
@@ -22,26 +22,70 @@
 """
     5.1.2 값 추가/삽입/삭제하기
 """
-subway = ["푸", "피글렛", "티거"]
+# subway = ["푸", "피글렛", "티거"]
 # print(type(subway))
 # print(subway)
 # print(subway.index("피글렛"))
 
-subway.append("이요르")
+# subway.append("이요르")
 # print(subway)
 
 # subway.insert(1, "루")
-subway.insert(subway.index("피글렛"), "루")
-print(subway)
+# subway.insert(subway.index("피글렛"), "루")
+# print(subway)
 
-print(subway.pop())
-print(subway)
+# print(subway.pop())
+# print(subway)
 
-print(subway.pop())
-print(subway)
+# print(subway.pop())
+# print(subway)
 
-print(subway.pop())
-print(subway)
+# print(subway.pop())
+# print(subway)
 
-subway.clear()
-print(subway)
+# subway.clear()
+# print(subway)
+
+"""
+    5.1.3 중복 값 확인하기
+"""
+# subway = ["푸", "피글렛", "티거"]
+# subway.append("푸")
+# print(subway)
+# print(subway.count("푸"))
+# subway.sort()
+# print(subway)
+
+"""
+    5.1.4 리스트 정렬하기
+"""
+# num_list = [5, 2, 4, 3, 1]
+# num_list.sort()
+# print(num_list)
+
+# num_list.sort(reverse = True)
+# print(num_list)
+
+# num_list.reverse()
+# print(num_list)
+
+# my_list = [1, 3, 2]
+# my_list.sort()
+# print(my_list)
+
+# your_list = [1, 3, 2]
+# new_list = sorted(your_list)
+# print(your_list)
+# print(new_list)
+
+"""
+    5.1.4 리스트 확장하기
+"""
+# mix_list = ["푸", 20, True, [5, 2, 4, 3, 1]]
+# print(mix_list)
+
+mix_list = ["푸", 20, True]
+num_list = [5, 2, 4, 3, 1]
+num_list.extend(mix_list)
+print(mix_list)
+print(num_list)
