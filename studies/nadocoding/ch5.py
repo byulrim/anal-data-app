9f1a91bec7c5c185c143758c0ee9a42190bb832d
byulrim <byulrim@gmail.com>
2025-12-24 01:55:45 +0900

    studying pythin w/ nadocoding(chapter 5; fourth time): anal-data-app project

--- studies/nadocoding/ch5.py
+++ studies/nadocoding/ch5.py
@@ -5,7 +5,7 @@
     5.1 리스트
 """
 """
-    5.1.1 리스트 생성하기
+        5.1.1 리스트 생성하기
 """
 # subway1 = 10
 # subway2 = 20
@@ -20,7 +20,7 @@
 # print(empty_list)
 
 """
-    5.1.2 값 추가/삽입/삭제하기
+        5.1.2 값 추가/삽입/삭제하기
 """
 # subway = ["푸", "피글렛", "티거"]
 # print(type(subway))
@@ -47,7 +47,7 @@
 # print(subway)
 
 """
-    5.1.3 중복 값 확인하기
+        5.1.3 중복 값 확인하기
 """
 # subway = ["푸", "피글렛", "티거"]
 # subway.append("푸")
@@ -57,7 +57,7 @@
 # print(subway)
 
 """
-    5.1.4 리스트 정렬하기
+        5.1.4 리스트 정렬하기
 """
 # num_list = [5, 2, 4, 3, 1]
 # num_list.sort()
@@ -79,13 +79,52 @@
 # print(new_list)
 
 """
-    5.1.4 리스트 확장하기
+        5.1.4 리스트 확장하기
 """
 # mix_list = ["푸", 20, True, [5, 2, 4, 3, 1]]
 # print(mix_list)
+# print("멍멍이" in mix_list)
 
-mix_list = ["푸", 20, True]
-num_list = [5, 2, 4, 3, 1]
-num_list.extend(mix_list)
-print(mix_list)
-print(num_list)
+# mix_list = ["푸", 20, True]
+# num_list = [5, 2, 4, 3, 1]
+# num_list.extend(mix_list)
+# print(mix_list)
+# print(num_list)
+
+"""
+    5.2 딕셔너리
+"""
+"""
+        5.2.1 딕셔너리 생성하기
+"""
+# cabinet = {3: "푸", 100: "피글렛"}
+
+# empty_dict = {}
+# print(type(empty_dict))
+
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet.get(101))   # 해당 키에 해당하는 데이터가 없으면 None 반환
+
+# print(cabinet.get(5))
+# print("hi")
+# print(cabinet[5])
+# print("hi")
+
+# print(cabinet.get(5, "사용가능"))
+
+# print(3 in cabinet)
+# print(5 in cabinet)
+
+# cabinet = {"A-3": "푸", "B-100": "피글렛"}
+# print(cabinet["A-3"])
+# print(cabinet["B-100"])
+
+# print("곰" in "곰돌이")
+# print("돌이" in "곰돌이")
+# print("푸" in "곰돌이")
+
+"""
+        5.2.2 값 변경/추가/삭제하기
+"""
