2b29737d3023bf661989c6791a6a710518f7e743
byulrim <byulrim@gmail.com>
2026-01-06 23:30:31 +0900

    studying python w/ nadocoding(chapter 8; first time): anal-data-app project

---
+++ studies/nadocoding/ch08.py
@@ -0,0 +1,12 @@
+"""
+8장 입출력
+"""
+"""
+    8.1 표준 입력받기: input()
+"""
+answer = input("아무 값이나 입력하세요: ")
+print("입력한 값은 " + answer + "입니다.")
+print(type(answer))
+print(type(int(answer)))
+answer = 10
+print(type(answer))
