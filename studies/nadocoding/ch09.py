662fa427dc5936e28920e4883dfb6ff2022601d0
byulrim <byulrim@gmail.com>
2026-01-10 23:18:27 +0900

    studying python w/ nadocoding(chapter 9; second time): anal-data-app project

--- studies/nadocoding/ch09.py
+++ studies/nadocoding/ch09.py
@@ -36,15 +36,99 @@
 """
     9.2 클래스와 객체 생성하기
 """
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print(f"{self.name} 유닛을 생성했습니다.")
+#         print(f"체력 {self.hp}, 공격력 {self.damage}")
+
+# soldier1 = Unit("보병", 40, 5)
+# soldier2 = Unit("보병", 40, 5)
+# tank = Unit("탱크", 160, 35)
+
+"""
+        9.2.1 생성자: __init__( )
+"""
+# soldier3 = Unit("보병")
+# soldier3 = Unit("보병", 40)
+
+"""
+        9.2.2 인스턴스 변수
+"""
+# stealth1 = Unit("전투기", 80, 5)
+# print("유닛 이름 : {}, 공격력 : {}".format(stealth1.name, stealth1.damage))
+# # stealth1.clocking = True
+
+# stealth2 = Unit("업그레이드한 전투기", 80, 5)
+# stealth2.clocking = True
+
+# if stealth2.clocking == True:
+#     print("{}는 현재 은폐 상태입니다.".format(stealth2.name))
+
+# # if stealth1.clocking == True:
+# #     print("{}는 현재 은폐 상태입니다.".format(stealth1.name))
+
+"""
+        9.2.3 메서드
+"""
+# class AttackUnit:
+#     def __init__(self, name: str, hp: int, damage:int):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+
+#     def attack(self, location: str) -> None:
+#         print("{} : {} 방향 적군을 공격합니다. [공격력 {}]" \
+#               .format(self.name, location, self.damage))
+
+#     def damaged(self, damage: int) -> None:
+#         print(f"{self.name} : {damage}만큼 피해를 입었습니다.")
+#         self.hp -= damage
+#         print(f"{self.name} : 현재 체력은 {self.hp}입니다.")
+#         if self.hp <= 0:
+#             print(f"{self.name} : 파괴됐습니다.")
+
+# flameThrower1 = AttackUnit("화염방사병", 50, 16)
+# flameThrower1.attack("5시")
+
+# flameThrower1.damaged(25)
+# flameThrower1.damaged(25)
+
+"""
+    9.3 클래스 상속(인헤리턴스)하기
+"""
+"""
+        9.3.1 상속이란
+"""
 class Unit:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name: str, hp: int):
         self.name = name
         self.hp = hp
+
+class AttackUnit(Unit):
+    def __init__(self, name: str, hp: int, damage: int):
+        Unit.__init__(self, name, hp)
         self.damage = damage
-        print(f"{self.name} 유닛을 생성했습니다.")
-        print(f"체력 {self.hp}, 공격력 {self.damage}")
 
-soldier1 = Unit("보병", 40, 5)
-soldier2 = Unit("보병", 40, 5)
-tank = Unit("탱크", 160, 35)
+    def attack(self, location: str) -> None:
+        print("{} : {} 방향 적군을 공격합니다. [공격력 {}]" \
+              .format(self.name, location, self.damage))
 
+    def damaged(self, damage: int) -> None:
+        print(f"{self.name} : {damage}만큼 피해를 입었습니다.")
+        self.hp -= damage
+        print(f"{self.name} : 현재 체력은 {self.hp}입니다.")
+        if self.hp <= 0:
+            print(f"{self.name} : 파괴됐습니다.")
+
+flameThrower1 = AttackUnit("화염방사병", 50, 16)
+flameThrower1.attack("5시")
+
+flameThrower1.damaged(25)
+flameThrower1.damaged(25)
+
+"""
+        9.3.2 다중 상속
+"""
